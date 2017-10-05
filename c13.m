4f2c6fcce0e7e6b66f4987528d0325eb05ce4b50
velvalery <31800867+velvalery@users.noreply.github.com>
2017-10-05 15:51:07 +0300

    Add files via upload

---
+++ c13.m
@@ -0,0 +1,18 @@
+fs = 1000;
+b = [1 2 1]/4;
+a = 1;
+n = 512;
+ecg = load('ecg117.dat');
+ecgDetr = detrend(ecg);
+y = filter(b, a, ecgDetr);
+t = (0:length(ecgDetr)-1)/fs;
+figure(1);
+subplot(3,1,1),plot(t,ecg), xlim([1.1 1.6]);
+title('Нефільтрованийсигнал');
+xlabel('Час, t'), ylabel('Aмплітуда');
+subplot(3,1,2),plot(t,ecgDetr), xlim([1.1 1.6]);
+title('Безпостійноїскладової');
+xlabel('Час, t'), ylabel('Aмплатуда');
+subplot(3,1,3),plot(t,y), xlim([1.1 1.6]);
+title('Фільтрованийсигнал');
+xlabel('Час, t'), ylabel('Aмплітуда');
